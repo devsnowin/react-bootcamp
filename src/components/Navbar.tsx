594f3691fc0f0cfdb64dcb03ecaf460e987ae989
im-snowin <hello.snowin@gmail.com>
2022-12-26 19:42:01 +0530

    initialized react router and created pages to navigate around the app.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,5 @@
-import {
-  AppBar,
-  IconButton,
-  Typography,
-  Link,
-  Box,
-  Stack,
-} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { AppBar, IconButton, Typography, Box, Stack } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -18,21 +12,21 @@ const Navbar = ({ handleOpen }: { handleOpen: any }) => {
         justifyContent='space-between'
         sx={{ paddingInline: '2rem' }}
       >
-        <Typography
-          component={Link}
-          href='/'
-          variant='h1'
-          fontSize='2em'
-          fontWeight='bold'
-          color='#fff'
-          textAlign='center'
-          my='2rem'
-          sx={{
-            textDecoration: 'none',
-          }}
-        >
-          Pet Store
-        </Typography>
+        <NavLink to='/'>
+          <Typography
+            variant='h1'
+            fontSize='2em'
+            fontWeight='bold'
+            color='#fff'
+            textAlign='center'
+            my='2rem'
+            sx={{
+              textDecoration: 'none',
+            }}
+          >
+            Pet Store
+          </Typography>
+        </NavLink>
         <Box>
           <IconButton onClick={handleOpen} color='inherit'>
             <SearchIcon fontSize='large' />
---
+++ src/pages/Home.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable react/no-array-index-key */
+import { Container, Stack } from '@mui/material';
+import { useState, useEffect } from 'react';
+
+import { getPet } from '../lib/pet';
+import useBreedList from '../lib/hooks';
+import Pet from '../components/Pet';
+import PetForm from '../components/PetForm';
+import Navbar from '../components/Navbar';
+
+const animals = ['cat', 'dog', 'bird', 'rabbit', 'reptile'];
+
+function Home() {
+  const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const fetchPets = async () => {
+    const data = await getPet(animal, location, breed);
+    setPets(data);
+  };
+
+  useEffect(() => {
+    fetchPets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleSubmit = () => {
+    fetchPets();
+    setIsOpen(false);
+  };
+
+  const handleAnimalChange = (e: any) => {
+    setAnimal(e.target.value);
+    setBreed('');
+  };
+
+  const handleBreedChange = (e: any) => {
+    setBreed(e.target.value);
+  };
+
+  const handleLocationChange = (e: any) => {
+    setLocation(e.target.value);
+  };
+
+  const handleClickOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Container>
+      <Navbar handleOpen={handleClickOpen} />
+      <PetForm
+        animals={animals}
+        breeds={breeds}
+        animal={animal}
+        location={location}
+        breed={breed}
+        isOpen={isOpen}
+        handleAnimalChange={handleAnimalChange}
+        handleLocationChange={handleLocationChange}
+        handleBreedChange={handleBreedChange}
+        handleSubmit={handleSubmit}
+        handleOpen={handleClickOpen}
+        handleClose={handleClose}
+      />
+      <Stack
+        component='div'
+        direction={{ md: 'row' }}
+        alignItems='center'
+        gap={{ md: '2rem' }}
+        flexWrap='wrap'
+        mt='8rem'
+        mb='4rem'
+      >
+        {pets ? (
+          pets.map((pet: Pet) => <Pet key={pet.id} pet={pet} />)
+        ) : (
+          <p>no data</p>
+        )}
+      </Stack>
+    </Container>
+  );
+}
+
+export default Home;
